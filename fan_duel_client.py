3834a975cc9e8bc0c24fd8dad3434aafdc3429ab
jaebradley <jae.b.bradley@gmail.com>
2017-01-20 02:17:09 -0500

    add client contests fetch method

--- fan_duel_client.py
+++ fan_duel_client.py
@@ -1,5 +1,16 @@
+import requests
+
+from utilities.builders import UrlBuilder, HeadersBuilder
+
+
 class FanDuelClient:
     def __init__(self, basic_authorization_header_value, x_auth_token_header_value):
-        self.basic_authorization_header_value = basic_authorization_header_value
-        self.x_auth_token_header_value = x_auth_token_header_value
+        self.headers = HeadersBuilder.build(basic_authorization_header_value=basic_authorization_header_value,
+                                            x_auth_token_header_value=x_auth_token_header_value)
 
+    def get_contests(self):
+        response = requests.get(url=UrlBuilder.build_contest_url(), headers=self.headers)
+
+        response.raise_for_status()
+
+        return response.json()
