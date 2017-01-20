ebbe6b2e8a1a69fdf494a10488b2f4db4f7634ed
jaebradley <jae.b.bradley@gmail.com>
2017-01-20 02:03:03 -0500

    add client

---
+++ fan_duel_client.py
@@ -0,0 +1,5 @@
+class FanDuelClient:
+    def __init__(self, basic_authorization_header_value, x_auth_token_header_value):
+        self.basic_authorization_header_value = basic_authorization_header_value
+        self.x_auth_token_header_value = x_auth_token_header_value
+
